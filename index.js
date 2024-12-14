4fd37140af6f3323b41e64fe28e3e6711fe09e89
Salahudin Saani <udinsalah003@gmail.com>
2024-12-14 11:15:45 +0500

    this is for ahmda

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+// const ahmad = "i am ahmad"
+
+// let ahmad2 = 345
+// ahmad3 = 2452435
+
+
+// var ahmad1 = 123
+
+// console.log(ahmad)
+// console.log(ahmad1)
+// console.log(ahmad2)
+// console.log(ahmad3)
+
+// let salahudin = "fdasdfas fasdflkjasdf  asdlkfjasdljf "
+
+// let ahmad = {name: "ahmad", rollNo: 123123, college: "faiq"}
+
+ahmad = 234589023859234
+
+let salahudin = 123 + ahmad
+console.log("BOth of them have bank balance:",salahudin)
+
+let khan = ["name", 12312, true, "ahmad"]
+
+
+// console.log(ahmad)
+console.log(khan)
+
